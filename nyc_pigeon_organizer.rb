4e761dbb213d3150f950b35b3c7cb6193ac87dd7
David Brennan <ddbren@gmail.com>
2017-11-06 18:48:27 -0500

    Pigeon sorting passes tests

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,21 @@
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeons_by_name = {}
+  data.each do |key, descriptor|
+    descriptor.each do |names|
+      names.last.each do |name|
+        if pigeons_by_name[name]
+          if pigeons_by_name[name][key]
+            pigeons_by_name[name][key] << names.first.to_s
+          else
+            pigeons_by_name[name][key] = [names.first.to_s]
+          end
+        else
+          pigeons_by_name[name] = {}
+          pigeons_by_name[name][key] =[names.first.to_s]
+        end
+      end
+    end
+  end
+  pigeons_by_name
 end
